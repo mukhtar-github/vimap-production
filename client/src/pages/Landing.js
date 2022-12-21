450fd2b7261c8ccd7e5a87f5bc95982c59b17711
Mukhtar <mukhtartanimugarba@gmail.com>
2022-12-21 07:06:22 +0100

    Update file

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -21,10 +21,10 @@ const Landing = () => {
               vehicle <span>information</span> app
             </h1>
             <p>
-              Working as a Fleet Manager with my employer, the challenge of
-              going through archives to get vehicle information for updates was
-              time-consuming. The challenge gave birth to VIMAP, an acronym for
-              Vehicles Information Management Application. The application
+              While working as a Fleet Manager with my employer, the challenge
+              of going through archives to get vehicle information for updates
+              was time-consuming. The challenge gave birth to VIMAP, an acronym
+              for Vehicles Information Management Application. The application
               provides a solution to replace the analog method of accessing,
               saving, and updating vehicle information.{" "}
               <a href="https://medium.com/@mukhtartanimugarba/the-vimap-manual-bb9d299cebba">
